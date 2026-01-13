7f6a1b7db20808faef92e6fbda7257cd495180cf
Kajerkooo2 <prokajerko79@gmail.com>
2026-01-13 08:44:18 +0100

    Przekreslenie itemu listy po klikneciu i od kliknicie, na 5

--- app/src/main/java/com/example/lista_zakupow/MainActivity.java
+++ app/src/main/java/com/example/lista_zakupow/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.lista_zakupow;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ArrayList<String> rzeczyDoZrobienia;
     private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<Boolean> wykonane;
+
 
     private Button button;
 
@@ -58,5 +64,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //to jest na 4, MOZE byc zamiast formularza cos z losowaniem, przecwiczyc
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(Color.GRAY);
+                TextView textView = (TextView) view;
+                if(textView.getPaintFlags() == Paint.STRIKE_THRU_TEXT_FLAG){
+                    textView.setPaintFlags(Paint.ANTI_ALIAS_FLAG);
+                }else{
+                    textView.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                }
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                rzeczyDoZrobienia.remove(i);
+                arrayAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
 }
