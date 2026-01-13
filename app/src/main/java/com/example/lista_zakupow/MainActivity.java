a022262f08a6c3ed026fe24e588b7a810e7a2a7c
Kajerkooo2 <prokajerko79@gmail.com>
2026-01-13 08:17:38 +0100

    Podłaczenie arrayAdaptera do listy, do sprawdzianu ocena - 3

--- app/src/main/java/com/example/lista_zakupow/MainActivity.java
+++ app/src/main/java/com/example/lista_zakupow/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lista_zakupow;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> rzeczyDoZrobienia;
+    private ArrayAdapter<String> arrayAdapter;
+
+    private Button button;
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.btnAdd);
+        editText = findViewById(R.id.etNewItem);
+
+        rzeczyDoZrobienia = new ArrayList<>();
+        rzeczyDoZrobienia.add("Wyjscie do kina");
+        rzeczyDoZrobienia.add("Nauczyc sie robienia czegos");
+        rzeczyDoZrobienia.add("Wyjdz z psem");
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,rzeczyDoZrobienia);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
